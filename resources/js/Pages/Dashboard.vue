e7391042ffc9807fe41f0ff53a17bd007010f05e
Charles Chadwick <charles@mutuallyassured.com>
2025-11-27 11:17:01 -0800

    Updating layout

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+import AuthenticatedLayout from "./AuthenticatedLayout.vue";
+</script>
+
+<template>
+<AuthenticatedLayout></AuthenticatedLayout>
+</template>
+
+<style scoped>
+
+</style>
