aab1c7d81d261556074b4d73e376e6c45e6a5ff8
Charles <charles@mutuallyassured.com>
2025-12-26 15:25:42 -0800

    Not sure about this. I need to create a separate activity controller. This will do for now.

--- resources/js/Components/Activity.vue
+++ resources/js/Components/Activity.vue
@@ -13,18 +13,11 @@ defineProps ( {
 <template>
   <p class="text-sm">
 
-    <template v-if="type === 'updated' && object.relationships?.updated_by">
-      Updated by: {{ object.relationships.updated_byfull_name }}
-      on {{ objectupdated_at }}
+    <template v-if="type === 'created' && object?.created_by">
+      Created by: {{ object.created_by.full_name }}
+      on {{ object.created_at }}
     </template>
-    <template v-else-if="type === 'deleted' && object.relationships?.deleted_by">
-      Deleted by: {{ object.relationships.deleted_byfull_name }}
-      on {{ objectdeleted_at }}
-    </template>
-    <template v-else>
-      Created by: {{ object.relationships.created_byfull_name }}
-      on {{ objectcreated_at }}
-    </template>
+
   </p>
 </template>
 
