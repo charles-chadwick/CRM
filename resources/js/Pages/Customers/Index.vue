028aa9c2153eb03c9bf1fe5b41fdc2f7660006fc
Charles Chadwick <charles@mutuallyassured.com>
2025-12-30 12:18:44 -0800

    Pagination

--- resources/js/Pages/Customers/Index.vue
+++ resources/js/Pages/Customers/Index.vue
@@ -35,51 +35,53 @@ const props = defineProps ( { customers: Object } );
         />
       </div>
       <Card>
-      <table class="min-w-full border-collapse table-auto">
-        <thead>
-        <tr class="table-header">
-          <th>Name</th>
-          <th>Job Title</th>
-          <th>Company</th>
-          <th>Actions</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr
-            v-for="customer in props.customers.data"
-            :key="customer.id"
-            class="table-row"
-        >
-          <td class="table-cell">
-            <CustomerDetails :customer="customer" />
-          </td>
-          <td class="table-cell">{{ customer.title }}</td>
-          <td class="table-cell click">
-            <CompanyDetails
-                v-if="customer?.company"
-                :company="customer?.company"
-            />
-          </td>
+        <table class="min-w-full border-collapse table-auto">
+          <thead>
+          <tr class="table-header">
+            <th>Name</th>
+            <th>Job Title</th>
+            <th>Company</th>
+            <th>Actions</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr
+              v-for="customer in props.customers.data"
+              :key="customer.id"
+              class="table-row"
+          >
+            <td class="table-cell">
+              <CustomerDetails :customer="customer" />
+            </td>
+            <td class="table-cell">{{ customer.title }}</td>
+            <td class="table-cell click">
+              <CompanyDetails
+                  v-if="customer?.company"
+                  :company="customer?.company"
+              />
+            </td>
 
-          <td class="table-cell">
-            <div class="flex gap-2 justify-center items-center">
-              <EditButton
-                  prefix="customers"
-                  :id="customer.id"
-              />
-              <DeleteButton
-                  prefix="customers"
-                  :id="customer.id"
-                  :message="customer.full_name"
-              />
-            </div>
-          </td>
-        </tr>
-        </tbody>
-      </table>
-      <Pagination
-          :pagination="props.customers"
-      />
+            <td class="table-cell">
+              <div class="flex gap-2 justify-center items-center">
+                <EditButton
+                    prefix="customers"
+                    :id="customer.id"
+                />
+                <DeleteButton
+                    prefix="customers"
+                    :id="customer.id"
+                    :message="customer.full_name"
+                />
+              </div>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+        <Pagination
+            :pagination="props.customers"
+            v-if="props.customers.data.length > 0"
+        />
+        <p class="p-4 text-center" v-else>No customers found.</p>
       </Card>
     </div>
   </AppLayout>
