503fff7e23bfe20cfb046f55b5ad8e26def2ce1b
Charles Chadwick <charles@mutuallyassured.com>
2025-12-31 06:25:40 -0800

    Formatting

--- app/Models/Discussion.php
+++ app/Models/Discussion.php
@@ -11,7 +11,7 @@ class Discussion extends Base
     use HasUsers;
 
     /**
-     * The attributes that are mass assignable.
+     * The attributes that are mass-assignable.
      *
      * @var array<int, string>
      */
